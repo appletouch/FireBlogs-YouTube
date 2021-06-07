f0a2359d688d5ce3161e41611d431807e911feaa
appletouch <bluedolfin.nl@gmail.com>
2021-06-07 23:11:21 +0200

    Blog Post Component (Home)

--- src/App.vue
+++ src/App.vue
@@ -60,4 +60,17 @@ export default {
 .link-light {
   color: #fff;
 }
+
+.arrow{
+  margin-left: 8px;
+  width: 12px;
+  path{
+    fill: #000000;
+  }
+}
+.arrow-light{
+  path{
+    fill: #ffffff;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="home">
-
+    <Blogpost :post="WelcomeScreen" />
+    <Blogpost :post="post" v-for="(post, index) in samplePosts" :key="index" />
   </div>
 </template>
 
 <script>
+import Blogpost from "@/components/BlogPost/BlogPost";
+
 export default {
   name: "Home",
-  components: {},
+  data(){
+    return{
+      WelcomeScreen:{
+        title: "Hello Everyone",
+        blogPost: "Welcome to everyone in this Blog",
+        isWelcomeScreen:true,
+        photo:"coding"
+      },
+      samplePosts:[
+        {
+          title: "This is title Posting NR1",
+          blogHTML:"this is blogpost 1",
+          blogCoverPhoto:"beautiful-stories"
+        },
+        {
+          title: "This is title Posting NR2",
+          blogHTML:"this is blogpost 2",
+          blogCoverPhoto:"designed-for-everyone"
+        },
+      ]
+    }
+  },
+  components: {
+    Blogpost,
+  },
 };
 </script>
